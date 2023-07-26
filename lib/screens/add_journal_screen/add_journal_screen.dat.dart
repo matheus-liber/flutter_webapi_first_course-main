2bb9578b2c9053917aeef2afc3d97a9f94e0c512
matheus-liber <matheuslibergt@gmail.com>
2023-07-26 14:48:02 -0300

    Initial commit

---
+++ lib/screens/add_journal_screen/add_journal_screen.dat.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/helpers/weekday.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import '../../models/journal.dart';
+
+class AddJournalScreen extends StatelessWidget {
+  final Journal journal;
+
+  final TextEditingController _contentController = TextEditingController();
+
+  AddJournalScreen({Key? key, required this.journal}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(WeekDay(journal.createdAt).toString()),
+        actions: [
+          IconButton(
+              onPressed: () {
+                registerJournal(context);
+              },
+              icon: const Icon(Icons.check)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          controller: _contentController,
+          keyboardType: TextInputType.multiline,
+          style: const TextStyle(fontSize: 24),
+          expands: true,
+          minLines: null,
+          maxLines: null,
+        ),
+      ),
+    );
+  }
+
+  registerJournal(BuildContext context) {
+    String content = _contentController.text;
+
+    journal.content = content;
+
+    JournalService service = JournalService();
+    service.register(journal).then((value) {
+      Navigator.pop(context, value);
+    });
+  }
+}

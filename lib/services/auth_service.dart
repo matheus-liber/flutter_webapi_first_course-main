47e2ab67f22d91d6a1f0b702def1f5cfccd63172
matheus-liber <matheuslibergt@gmail.com>
2023-08-08 11:50:11 -0300

    Criando funcionalidade de login e registro

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/intercepted_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'http_interceptors.dart';
 
@@ -30,6 +31,8 @@ class AuthService {
       }
       throw HttpException(response.body);
     }
+    
+    saveUserInfos(response.body);
 
     return true;
   }
@@ -42,6 +45,29 @@ class AuthService {
           'password': password,
         }
     );
+    if(response.statusCode != 201){
+      throw HttpException(response.body);
+    }
+
+    saveUserInfos(response.body);
+  }
+  
+  saveUserInfos(String body) async {
+    Map<String, dynamic> map = json.decode(body);
+
+    String token = map["accessToken"];
+    String email = map["user"]["email"];
+    int id = map["user"]["id"];
+
+    //print("$token\n$email\n$id");
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("accessToken", token);
+    prefs.setString("email", email);
+    prefs.setInt("id", id);
+
+    String? tokenSalvo = prefs.getString("accessToken");
+    print(tokenSalvo);
   }
 }
 
